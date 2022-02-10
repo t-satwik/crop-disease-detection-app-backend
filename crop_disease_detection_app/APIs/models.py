caac3baccbba5506d2036b33dedfa079eea78766
t-satwik <tallurisatvik800@gmail.com>
2022-02-10 12:13:06 +0530

    Updated Data Model, with image_path and image fields

--- crop_disease_detection_app/APIs/models.py
+++ crop_disease_detection_app/APIs/models.py
@@ -31,6 +31,13 @@ def upload_to(instance, folder_path):
     file_name=user_name+'_'+crop_type+"_"+time_stamp+'.png'
     return folder_path+file_name
 
+def clean_str(var):
+    var.replace('/', '-')
+    var.replace(' ', '_')
+    var.replace(':', '-')
+    return var
+def upload_to(instance, filename):
+    return 'images/{filename}'.format(filename=filename)
 class Data(models.Model):
     time_stamp = models.TextField(null=False)
     latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=False)
@@ -39,9 +46,22 @@ class Data(models.Model):
     probability = models.DecimalField(max_digits=22, decimal_places=16, blank=False, null=False)
     user=models.ForeignKey(User, blank=False, on_delete=models.CASCADE, null=True)
     crop_type=models.ForeignKey(Crop, blank=False, on_delete=models.CASCADE, null=True)
+    def get_image_path(self, folder_path):
+        ts=clean_str(self.time_stamp)
+        un=clean_str(self.user.user_name)
+        ct=clean_str(self.crop_type.crop_name)
+        file_name=un+'_'+ct+"_"+ts+'.png'
+        return folder_path+file_name
+    image_path=models.TextField(blank=True, null=True) #In api calls while making an object, keep image_path=Data.get_image_path(folder_path)
+    image=image=models.ImageField(_("IMAGE"), blank=True, null=True, upload_to=upload_to, default='images/default.png')
+    def save(self, *args, **kwargs):
+        # Make sure this is the first save (pk should be None) and there is no unit_price set
+        if self.pk is None and not self.unit_price:
+            self.image_path = self.get_image_path('images/')
+        # Call the original save method
+        super(Data, self).save(*args, **kwargs)
     # image_path=upload_to(folder_path='media/')
     # image_path=models.TextField(blank=False)
-    # image=models.ImageField(_("IMAGE"), upload_to=image_path, default='images/default.png')
     
 
 # class VideoFrame(models.Model):
